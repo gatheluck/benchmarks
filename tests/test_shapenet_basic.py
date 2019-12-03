1b76c5c71d5a5910a1d2cacd7e2ff12a3aa19afd
gatheluck <gatheluck+tech@gmail.com>
2019-12-03 17:08:51 +0900

    add test for shapenet

--- tests/test_shapenet_basic.py
+++ tests/test_shapenet_basic.py
@@ -6,13 +6,30 @@ sys.path.append(base)
 
 import torch
 
-from kaolin.datasets.shapenet import ShapeNet_Images
+#from kaolin.datasets.shapenet import ShapeNet_Images
+from modules.shapenet import ShapeNetDataset 
 
-def test_shapenet_images():
-	dataset_root = '/home/gatheluck/Scratch/benchmarks/data/ShapeNetRendering'
-	categories = ['02691156']
-	dataset = ShapeNet_Images(dataset_root, categories, transform=None, split=1.0)
+# def test_shapenet_images():
+# 	dataset_root = '/home/gatheluck/Scratch/benchmarks/data/ShapeNetRendering'
+# 	categories = ['02691156']
+# 	dataset = ShapeNet_Images(dataset_root, categories, transform=None, split=1.0)
 	
+# 	loader = torch.utils.data.DataLoader(dataset, batch_size=2, num_workers=8)
+
+# 	for i, batch in enumerate(loader):
+# 		data = batch['data']
+# 		attributes = batch['attributes']
+# 		print(data['images'].shape)
+# 		print(data['params'])
+# 		print(attributes)
+		
+# 	raise NotImplementedError
+
+def test_shapenet_dataset():
+	dataset_root = '/home/gatheluck/Scratch/benchmarks/data/ShapeNetSingle'
+	categories = ['02691156']
+	dataset = ShapeNetDataset(dataset_root, categories, transform=None, split=1.0)
+
 	loader = torch.utils.data.DataLoader(dataset, batch_size=2, num_workers=8)
 
 	for i, batch in enumerate(loader):
